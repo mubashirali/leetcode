d530122fb3e4838215e828ce6328275b5ae48412
Mubashir Ali <mubashirali@users.noreply.github.com>
2025-05-02 22:43:29 +0200

    Update MinPathSum.java

--- src/main/java/leet/code/solution/MinPathSum.java
+++ src/main/java/leet/code/solution/MinPathSum.java
@@ -3,6 +3,7 @@ package leet.code;
 public class MinPathSum {
 
     /**
+     * https://leetcode.com/problems/minimum-path-sum/description/
      * 64. Minimum Path Sum
      * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
      * <p>
